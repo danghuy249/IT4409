31fcaf64b74e30f892d456ea6ffd34b76e22b42b
DESKTOP-7JAEQO3\Dell <ndhuy3000ride@gmail.com>
2023-12-06 22:14:11 +0700

    create admin page, admin product, create product

--- FE/src/pages/AdminPage/AdminPage.jsx
+++ FE/src/pages/AdminPage/AdminPage.jsx
@@ -1,12 +1,57 @@
-import React from 'react';
+import { Menu } from 'antd'
+import React, { useState } from 'react'
+import { getItem } from '../../utils';
+import { UserOutlined, AppstoreOutlined } from '@ant-design/icons'
+import HeaderComponent from '../../components/HeaderComponent/HeaderComponent';
+import AdminUser from '../../components/AdminUser/AdminUser';
+import AdminProduct from '../../components/AdminProduct/AdminProduct.jsx';
 
 const AdminPage = () => {
-    return (
-        <div>
-            admin page
+  const items = [
+    getItem('Người dùng', 'user', <UserOutlined />),
+    getItem('Sản phẩm', 'product', <AppstoreOutlined />)
+  ];
+
+  const [keySelected, setKeySelected] = useState('')
+
+  const renderPage = (key) => {
+    switch(key) {
+      case 'user':
+        return (
+          <AdminUser />
+        )
+      case 'product':
+        return (
+          <AdminProduct />
+        )
+      default:
+        return <></>
+    }
+  }
+
+  const handleOnCLick = ({ key }) => {
+    setKeySelected(key)
+  }
+  console.log('keySelected', keySelected)
+  return (
+    <>
+      <HeaderComponent isHiddenSearch isHiddenCart />
+      <div style={{ display: 'flex', }}>
+        <Menu
+          mode="inline"
+          style={{
+            width: 256,
+            boxShadow: '1px 1px 2px #ccc',
+          }}
+          items={items}
+          onClick={handleOnCLick}
+        />
+        <div style={{ flex: 1, padding: '15px' }}>
+          {renderPage(keySelected)}
         </div>
-    );
+      </div>
+    </>
+  )
 }
 
-export default AdminPage;
-
+export default AdminPage
